1c148d4125f65ae4d380bda14d958250cff58f08
Yamasaku-yu <yamazkaiyusaku0731@yahoo.co.jp>
2025-06-01 07:10:10 +0900

    メッセージ画面編集

--- my-next-app/src/app/message/page.jsx
+++ my-next-app/src/app/message/page.jsx
@@ -12,22 +12,30 @@ const Message = () => {
   };
   return (
     <div className="container custom-container mt-5">
-      <form onSubmit={handleSubmit} className="mb-4 fixed-bottom row">
-        <div className="col ms-2">
-          <textarea
-            className="form-control form-control-lg"
-            rows="2"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            placeholder="メッセージを入力"
-          />
-        </div>
-        <div className="col-auto">
-          <button type="submit" className="btn btn-lg btn-primary mt-3 me-2">
-           送信
-          </button>
-        </div>
-      </form>
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          width: "80%"
+        }}
+      >
+        <form onSubmit={handleSubmit} className="mb-4 row">
+          <div className="col ms-2">
+            <textarea
+              className="form-control form-control-lg"
+              rows="2"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="メッセージを入力"
+            />
+          </div>
+          <div className="col-auto">
+            <button type="submit" className="btn btn-lg btn-primary mt-3">
+              送信
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
