6a7f5e5bf159ce59dfb8539207b4533bfa42a84f
Yamasaku-yu <yamazkaiyusaku0731@yahoo.co.jp>
2025-06-01 13:09:36 +0900

    ページ移動エラー修正

--- my-next-app/src/app/layout.jsx
+++ my-next-app/src/app/layout.jsx
@@ -11,45 +11,48 @@ export default function RootLayout({ children }) {
               <span className="navbar-brand mb-0 h1">大学専用SNS</span>
             </div>
           </nav>
-          <div className="dropdown mt-4 ms-4">
-            <button
-              className="btn btn-secondary btn-lg"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              &#9776;
-            </button>
-            <ul className="dropdown-menu" style={{ width: 'auto', minWidth: 'unset' }}>
-              <li>
-                <a className="dropdown-item" href="/profile">
-                  プロフィール
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="/">
-                  ホーム
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="/search">
-                  検索
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="/message">
-                  メッセージ
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="/login">
-                  ログイン
-                </a>
-              </li>
-            </ul>
-          </div>{" "}
         </header>
-        <main className="mt-5 pt-2">{children}</main>
+        <div className="dropdown mt-5 ms-4 pt-2" style={{ width: "50px" }}>
+          <button
+            className="btn btn-secondary btn-lg"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            &#9776;
+          </button>
+          <ul
+            className="dropdown-menu"
+            style={{ width: "auto", minWidth: "unset" }}
+          >
+            <li>
+              <a className="dropdown-item" href="/profile">
+                プロフィール
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="/">
+                ホーム
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="/search">
+                検索
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="/message">
+                メッセージ
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="/login">
+                ログイン
+              </a>
+            </li>
+          </ul>
+        </div>
+        <main className="">{children}</main>
       </body>
     </html>
   );
