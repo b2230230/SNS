9f064f9a3a656907e2bd7ce19ae7244723eda153
ito_b2230230 <b2230230@photon.chitose.ac.jp>
2025-06-01 12:53:40 +0900

    投稿画面のユーザアイコンからプロフィール画面に移動できるように変更

--- my-next-app/src/app/page.jsx
+++ my-next-app/src/app/page.jsx
@@ -1,19 +1,22 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link"; // ← Next.js のリンクコンポーネント
 
 const Home = () => {
   const postData = [
     {
       id: 1,
+      userId: "1",
       name: "a大学",
       content: "こんにちは！",
       image:
         "https://user0514.cdnw.net/shared/img/thumb/aig-ai221017313-xl_TP_V.jpg",
     },
-    { id: 2, name: "b大学", content: "useState便利だね", image: null },
+    { id: 2, userId: "2", name: "b大学", content: "useState便利だね", image: null },
     {
       id: 3,
+      userId: "3",
       name: "c大学",
       content: "クラスだけで簡単に装飾できます。",
       image: null,
@@ -38,15 +41,19 @@ const Home = () => {
             return (
               <div key={post.id} className="card mb-1">
                 <div className="card-body">
-                  <h6 className="card-title">
-                    <img
-                      src="https://user0514.cdnw.net/shared/img/thumb/aig-ai230531008-xl_TP_V.jpg"
-                      alt="アイコン"
-                      className="rounded-circle me-2"
-                      width="30"
-                      height="30"
-                    />
-                    {post.name}
+                  <h6 className="card-title d-flex align-items-center">
+                    <Link href={`/profile`} className="text-decoration-none text-dark">
+                      <div className="d-flex align-items-center">
+                        <img
+                          src="https://user0514.cdnw.net/shared/img/thumb/aig-ai230531008-xl_TP_V.jpg"
+                          alt="アイコン"
+                          className="rounded-circle me-2"
+                          width="30"
+                          height="30"
+                        />
+                        {post.name}
+                      </div>
+                    </Link>
                   </h6>
                   <p className="card-text">{post.content}</p>
                   {post.image && (
